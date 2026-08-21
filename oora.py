bfcb849dd0e0963f9828846931d9ae5ebcb4989e
mimooh <mimoohowy@gmail.com>
2026-08-21 23:31:11 +0200

    null

--- oora.py
+++ oora.py
@@ -10,12 +10,12 @@ from prettytable import PrettyTable
 from datetime import datetime
 from subprocess import Popen, PIPE
 
-cx_Oracle.init_oracle_client() # thick mode for old orace
+# cx_Oracle.init_oracle_client() # thick mode for old orace
 
 class Oora:
     def __init__(self):# {{{
         #self.con=cx_Oracle.connect(os.environ['OORA_USER']      , os.environ['OORA_PASS']          , os.environ['OORA_HOST'])
-        self.con=cx_Oracle.connect(user=os.environ['OORA_USER'] , password=os.environ['OORA_PASS'] , dsn=os.environ['OORA_HOST']+"/USOS")
+        self.con=cx_Oracle.connect(user=os.environ['OORA_USER'] , password=os.environ['OORA_PASS'] , dsn=os.environ['OORA_HOST']+"/"+os.environ['OORA_SCHEMA'])
         self.cur=self.con.cursor()
         self.delimiter=';'
         self.aligned=False
